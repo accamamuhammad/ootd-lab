ccc5e5da59b621ddb123f6b62b0387220baf6441
accama <accamamuhammad@gmail.com>
2025-10-12 23:03:55 +0100

    outfits section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,51 @@
+"use client";
+
+import { useState } from "react";
+import { useEffect } from "react";
+
+import Nav from "../app/components/Nav";
+
+import Clothes from "../app/components/Clothes";
+import Outfit from "../app/components/Outfits";
+
 export default function Home() {
+  const [togglePages, setTogglePages] = useState(true);
+
+  const handleTogglePages = () => {
+    setTogglePages(!togglePages);
+  };
+
   return (
-    <h1 className="text-3xl text-purple-300 font-bold underline">
-      Hello world!
-    </h1>
+    <main className="w-full h-[100dvh] overflow-hidden flex flex-col items-center justify-start">
+      <section className="w-full space-y-4 z-50">
+        <Nav />
+        <div className="w-full flex flex-row items-center justify-center">
+          <h1
+            className={`${"w-1/2 border-b-2 pb-3 text-center cursor-pointer"} ${
+              togglePages ? "border-b-black" : "border-b-white"
+            }`}
+            onClick={handleTogglePages}
+          >
+            Clothes
+          </h1>
+          <h1
+            className={`${"w-1/2 border-b-2 pb-3 text-center cursor-pointer"} ${
+              togglePages ? "border-b-white" : "border-b-black"
+            }`}
+            onClick={handleTogglePages}
+          >
+            Outfits
+          </h1>
+        </div>
+      </section>
+      <section className="w-full h-full overflow-scroll scrollbar-hide">
+        <div className={togglePages ? "block" : "hidden"}>
+          <Clothes />
+        </div>
+        <div className={togglePages ? "hidden" : "block"}>
+          <Outfit />
+        </div>
+      </section>
+    </main>
   );
 }
